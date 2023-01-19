35a89915e07132deeeee2ca773722ae2faaaf2fe
G <G@DESKTOP-6ORDNF9>
2023-01-19 23:29:59 +0530

    delete contact

---
+++ VtigerForGitHub/src/test/java/contacts/DeleteContact_Test.java
@@ -0,0 +1,10 @@
+package contacts;
+
+import org.testng.annotations.Test;
+
+public class DeleteContact_Test {
+	@Test
+	public void deleteContact() {
+		System.out.println("contact is deleted");
+	}
+}
