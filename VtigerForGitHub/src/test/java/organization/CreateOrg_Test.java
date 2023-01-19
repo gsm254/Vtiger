dde426090bd11f65b3bccb7f8019efd7b7eb8e65
G <G@DESKTOP-6ORDNF9>
2023-01-19 22:13:10 +0530

    class is added

---
+++ VtigerForGitHub/src/test/java/organization/CreateOrg_Test.java
@@ -0,0 +1,10 @@
+package organization;
+
+import org.testng.annotations.Test;
+
+public class CreateOrg_Test {
+	@Test
+	public void create() {
+		System.out.println("Creating the organization");
+	}
+}
